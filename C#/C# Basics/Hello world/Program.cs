5609950ace2b0600c1d9a2dc3324f06fe6b63a96
Matyas.Horky <horkyskolni@gmail.com>
2023-10-10 12:16:38 +0200

    C# Basics

---
+++ C#/C# Basics/Hello world/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 0;            
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            String firstName = "name";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("\n{0} {1}", byte.MinValue, byte.MaxValue);
+
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            try
+            {
+                var number2 = "1234";
+                byte by = Convert.ToByte(number2);
+                Console.WriteLine(by);
+            } catch (Exception e){
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
